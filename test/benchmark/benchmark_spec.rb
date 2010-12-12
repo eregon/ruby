d26ecae0880b6de11a38387c99ce17487fc2b14b
eregon <eregontp@gmail.com>
2010-12-12 22:10:21 +0100

    lib/benchmark: add a few tests for Benchmark::Tms output

--- test/benchmark/benchmark_spec.rb
+++ test/benchmark/benchmark_spec.rb
@@ -12,6 +12,13 @@ describe Benchmark do
     [tf+tt+tu, (tf+tt+tu)/3]
   end
 
+  BENCH_FOR_TIMES_UPTO_NO_LABEL = lambda do |x|
+    n = 1000
+    x.report { for i in 1..n; a = "1"; end }
+    x.report { n.times do   ; a = "1"; end }
+    x.report { 1.upto(n) do ; a = "1"; end }
+  end
+
   def labels
     %w[first second third]
   end
@@ -34,6 +41,15 @@ describe Benchmark do
 
   def capture_bench_output(type, *args, &block)
     capture_output { bench(type, *args, &block) }
+  end
+
+  describe 'Tms' do
+    it 'outputs nicely' do
+      Benchmark::Tms.new.to_s.must_equal            "  0.000000   0.000000   0.000000 (  0.000000)\n"
+      Benchmark::Tms.new(1,2,3,4,5).to_s.must_equal "  1.000000   2.000000  10.000000 (  5.000000)\n"
+      Benchmark::Tms.new(1,2,3,4,5,'label').format('%u %y %U %Y %t %r %n').must_equal \
+        "1.000000 2.000000 3.000000 4.000000 10.000000 (5.000000) label"
+    end
   end
 
   describe 'benchmark' do
@@ -65,6 +81,10 @@ describe Benchmark do
 
     it 'correctly output when the label width is given' do
       capture_bench_output(:bm, 6).must_equal BM_OUTPUT
+    end
+
+    it 'correctly output when no label is given' do
+      capture_bench_output(:bm, &BENCH_FOR_TIMES_UPTO_NO_LABEL).must_equal BM_OUTPUT_NO_LABEL
     end
 
     it 'can make extra calcultations with an array at the end of the benchmark' do
@@ -100,6 +120,13 @@ BM_OUTPUT = <<BENCH
 first   --time--   --time--   --time-- (  --time--)
 second  --time--   --time--   --time-- (  --time--)
 third   --time--   --time--   --time-- (  --time--)
+BENCH
+
+BM_OUTPUT_NO_LABEL = <<BENCH
+      user     system      total        real
+  --time--   --time--   --time-- (  --time--)
+  --time--   --time--   --time-- (  --time--)
+  --time--   --time--   --time-- (  --time--)
 BENCH
 
 BMBM_OUTPUT = <<BENCH
