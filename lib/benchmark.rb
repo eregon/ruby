c3a5c5fc2906b0b5b38fdf0375e8bc9e78a62005
Benoit Daloze <eregontp@gmail.com>
2012-06-19 19:27:27 +0200

    lib/benchmark.rb: update included test

--- lib/benchmark.rb
+++ lib/benchmark.rb
@@ -554,7 +554,7 @@ if __FILE__ == $0
 
   n = ARGV[0].to_i.nonzero? || 50000
   puts %Q([#{n} times iterations of `a = "1"'])
-  benchmark("       " + CAPTION, 7, FORMAT) do |x|
+  benchmark(CAPTION, 7, FORMAT) do |x|
     x.report("for:")   {for _ in 1..n; _ = "1"; end} # Benchmark.measure
     x.report("times:") {n.times do   ; _ = "1"; end}
     x.report("upto:")  {1.upto(n) do ; _ = "1"; end}
