da93ce0e28087933b435d087a316cd2492581005
Benoit Daloze <eregontp@gmail.com>
2021-03-08 15:18:40 +0100

    Fixes for sync_default_gems

--- tool/sync_default_gems.rb
+++ tool/sync_default_gems.rb
@@ -109,7 +109,8 @@ $repositories = {
 def sync_default_gems(gem)
   puts "Sync #{$repositories[gem.to_sym]}"
 
-  upstream = File.join("..", "..", $repositories[gem.to_sym])
+  upstream = File.join("..", "..", File.basename($repositories[gem.to_sym]))
+  p upstream
 
   case gem
   when "rubygems"
@@ -350,23 +351,24 @@ def sync_default_gems_with_commits(gem, range)
 end
 
 def sync_lib(repo)
-  unless File.directory?("../#{repo}")
-    abort %[Expected '../#{repo}' \(#{File.expand_path("../#{repo}")}\) to be a directory, but it wasn't.]
+  upstream = "../../#{repo}"
+  unless File.directory?(upstream)
+    abort %[Expected '#{upstream}' \(#{File.expand_path(upstream)}\) to be a directory, but it wasn't.]
   end
   rm_rf(["lib/#{repo}.rb", "lib/#{repo}/*", "test/test_#{repo}.rb"])
-  cp_r(Dir.glob("../#{repo}/lib/*"), "lib")
+  cp_r(Dir.glob("#{upstream}/lib/*"), "lib")
   tests = if File.directory?("test/#{repo}")
             "test/#{repo}"
           else
             "test/test_#{repo}.rb"
           end
-  cp_r("../#{repo}/#{tests}", "test") if File.exist?("../#{repo}/#{tests}")
+  cp_r("#{upstream}/#{tests}", "test") if File.exist?("#{upstream}/#{tests}")
   gemspec = if File.directory?("lib/#{repo}")
               "lib/#{repo}/#{repo}.gemspec"
             else
               "lib/#{repo}.gemspec"
             end
-  cp_r("../#{repo}/#{repo}.gemspec", "#{gemspec}")
+  cp_r("#{upstream}/#{repo}.gemspec", "#{gemspec}")
 end
 
 def update_default_gems(gem)
